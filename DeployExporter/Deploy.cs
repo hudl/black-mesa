a5c4a6fb5ab0b063e52d00b2c03415b26104297d
Brian Clymer <crdnilfan@gmail.com>
2013-03-22 11:03:23 -0500

    Deploy exporter!

---
+++ DeployExporter/Deploy.cs
@@ -0,0 +1,55 @@
+﻿using CsvHelper.Configuration;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeployExporter
+{
+    [CollectionName("deploys")]
+    class Deploy : IEntity
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonIgnoreIfNull]
+        [CsvField(Ignore = true)]
+        public string Id { get; set; }
+        [CsvField(Ignore = true)]
+        public string LineNumber { get; set; }
+        public string Day { get; set; }
+        public string DateTime { get; set; }
+        public string Action { get; set; }
+        public string Component { get; set; }
+        public string Type { get; set; }
+        public string Project { get; set; }
+        public string Branch { get; set; }
+        public string PullRequest { get; set; }
+        public string PullRequestLink{ get; set; }
+        public string Jira { get; set; }
+        public string QA { get; set; }
+        public string Des { get; set; }
+        public string Dev { get; set; }
+        public string DevCodeReview { get; set; }
+        public string ProjectManager { get; set; }
+        public string Notes { get; set; }
+
+        public bool IsHotfix { get; set; }
+
+        public string Special { get; set; }
+        public string DevTeamCulpability { get; set; }
+        public string DevHudlWideImpact { get; set; }
+        public string DevAffectedUserImpact { get; set; }
+        public string DevInitials { get; set; }
+        public string QaTeamCulpability { get; set; }
+        public string QaHudlWideImpact { get; set; }
+        public string QaAffectedUserImpact { get; set; }
+        public string QaInitials { get; set; }
+        public string BranchThatBrokeIt { get; set; }
+        public string ProdTicketNum { get; set; }
+        public string ProdTicket { get; set; }
+        public string HfNotes { get; set; }
+    }
+}
---
+++ DeployExporter/Program.cs
@@ -0,0 +1,25 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeployExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repo = new DeployRepository();
+            var deploys = repo.Collection.FindAll().ToList();
+            using (var csv = new CsvWriter(new StreamWriter("black-mesa.tsv")))
+            {
+                csv.Configuration.Delimiter = "\t";
+                csv.WriteRecords<Deploy>(deploys);
+            }
+            Console.Read();
+        }
+    }
+}
